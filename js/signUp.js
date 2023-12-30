7e3fdfd337a63c68e22df5e2790aaa3f5f47b819
tomi <tomasgroso09@gmail.com>
2023-12-30 13:28:53 -0300

    comienzo rama sign up y login

---
+++ js/signUp.js
@@ -0,0 +1,11 @@
+const email = document.querySelector("#emailText")
+const nombre = document.querySelector("#nombre")
+const password = document.querySelector("#password")
+const users = []
+
+email,nombre,password.addEventListener("keyup",()=>{
+    const emailUsuario = email.value
+    const nombreUsuario = nombre.value
+    const passwordUsuario = password.value
+
+})
