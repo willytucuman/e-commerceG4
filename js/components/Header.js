0ed248cf749e19a540a4c59d593e0a130f18a3a4
Facundo Gimenez <facuie014@gmail.com>
2024-01-04 22:09:02 -0300

    new navBar style

--- js/components/Header.js
+++ js/components/Header.js
@@ -1,55 +1,37 @@
+
 const header = document.querySelector("header");
+const head = document.querySelector("head")
 
 const Navbar = () => {
+    const menuIcon = document.querySelector(".menu-icon")
+    const navList = document.querySelector("ul")
+
+    menuIcon.addEventListener("click",()=>{
+        menuIcon.classList.toggle("active")
+        navList.classList.toggle("active")
+})
     return (header.innerHTML = `
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top ">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="./assets/logo/logo_Minimizado.png" alt="Logo" width="40" height="32" class="d-inline-block align-text-center">
-            HardTech
-        </a>
-        <button class="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="offcanvas" 
-            data-bs-target="#offcanvasDarkNavbar" 
-            aria-controls="offcanvasDarkNavbar" 
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title " id="offcanvasDarkNavbarLabel"><img src="./assets/logo/logo_Minimizado.png" alt="Logo" width="40" height="32" class="d-inline-block align-text-center  ">
-                    HardTech
-                </h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <nav>
+            <div class="logo">
+                <a href="">HardTech</a>
             </div>
-            <div class="offcanvas-body">
-                <ul class="navbar-nav  justify-content-start align-items-center flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lista de Deseos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">CRUD</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav justify-content-end align-items-center   flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active ms-2 me-2 p-0" aria-current="page" href="#" alt="log-in"><i class="bi bi-person-circle fs-2 p-0"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link p-0 ms-2 me-2" href="#"><i class="bi bi-cart fs-2 p-0 "></i></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Whishlist</a></li>
+                <li><a href="">CRUD</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+            <div class="menu-icon"></div>
+        </nav>
     `)
+    (head.innerHTML ='<link rel="stylesheet" href="./css/navBar.css">')
 }
 export default Navbar;
+const menuIcon = document.querySelector(".menu-icon")
+const navList = document.querySelector("ul")
+
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active")
+    navList.classList.toggle("active")
+})
