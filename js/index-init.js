c0c69a9b54bdd4024fd7b118b083253caa24cd66
FedericoFrau <fedeafrau@gmail.com>
2024-01-14 03:56:06 -0300

    integral footer code modifications in index.html, temp disable index-init.js

--- js/index-init.js
+++ js/index-init.js
@@ -1,5 +1,15 @@
-import Footer from "./components/Footer.js";
+// import Footer from "./components/Footer.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    Footer();
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     Footer();
+// });
+
+
+// document.getElementsByTagName("footer")[0].classList.value = "container";
+
+
+// const div1 = document.getElementsByTagName("footer")[0];
+// console.log(div1)
+// const align = document.getElementsByTagName("footer")[0].getAttribute("class")
+// console.log(align);
+// document.getElementsByTagName("footer")[0].setAttribute("class", "container-fluid");
