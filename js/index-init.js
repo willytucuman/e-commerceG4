8fb5faefbeb5dd816ec1f714414349117caf14d2
FedericoFrau <fedeafrau@gmail.com>
2024-01-14 22:52:58 -0300

    restructuring and correct footer code in component

--- js/index-init.js
+++ js/index-init.js
@@ -1,15 +1,11 @@
-// import Footer from "./components/Footer.js";
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     Footer();
-// });
-
-
-// document.getElementsByTagName("footer")[0].classList.value = "container";
+import Footer from "./components/Footer.js";
+const head = document.getElementsByTagName("head")[0];
+const title = document.getElementsByTagName("title")[0];
+const headFaviconContent = `<link rel="icon" type="image/icon" sizes="" href="./assets/favicon/favicon-red-48px-HT.ico" />`;
 
 
-// const div1 = document.getElementsByTagName("footer")[0];
-// console.log(div1)
-// const align = document.getElementsByTagName("footer")[0].getAttribute("class")
-// console.log(align);
-// document.getElementsByTagName("footer")[0].setAttribute("class", "container-fluid");
+document.addEventListener("DOMContentLoaded", () => {
+    head.insertAdjacentHTML("beforeend", headFaviconContent);
+    title.innerHTML = "HARD-TECH";
+    Footer();
+});
