64f1ee5717ea6ab221ffb0038974053150dccc9c
jeremiasjm <jeremiasjuarezmarrades15@gmail.com>
2024-01-17 23:12:46 -0300

    agrege funcion sep y get product

---
+++ js/classes/product-class.js
@@ -0,0 +1,11 @@
+export class Product {
+    constructor(id, image, title, description, price, category,stock) {
+      this.id = id;
+      this.image = image;
+      this.title = title;
+      this.description = description;
+      this.price = price;
+      this.category = category;
+      this.stock = stock;
+    }
+  }
---
+++ js/services/getProducts.js
@@ -0,0 +1,3 @@
+export const getProducts = () => {
+    return JSON.parse(localStorage.getItem('products')) 
+}
