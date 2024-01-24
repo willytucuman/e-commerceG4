02d583454a4efee75484bc96d19406aec7febd37
FernandezCristian <fernandezcristian.utn@gmail.com>
2024-01-24 15:02:13 -0300

    About para completar con los datos de redes

--- js/about.js
+++ js/about.js
@@ -1,3 +1,15 @@
+import About from "./components/About copy.js";
+const head = document.getElementsByTagName("head")[0];
+const title = document.getElementsByTagName("title")[0];
+
+  About();
+  document.addEventListener("DOMContentLoaded", () => {
+  head.insertAdjacentHTML("beforeend", headFaviconContent);
+  title.innerHTML = "HARD-TECH";
+  About();
+});
+
+
 const menu=document.querySelector(".menu");
 const toggle=document.querySelector(".toggle");
 toggle.addEventListener("click",()=>{
