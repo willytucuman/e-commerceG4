c5e898c8532f830fe8647e6f1256601ef3aa0349
FedericoFrau <fedeafrau@gmail.com>
2024-01-25 00:45:11 -0300

    filter component code completed and corrected

--- js/index.js
+++ js/index.js
@@ -13,11 +13,12 @@ let products = getProducts()
 document.addEventListener("DOMContentLoaded", () => {
   head.insertAdjacentHTML("beforeend", headFaviconContent);
   title.innerHTML = "HARD-TECH";
-  Footer();
   ProductsFilter()
+  loadTable(products);
+  Footer();
 });
 
-const loadTable = (objectsArray) => {
+export const loadTable = (objectsArray) => {
   productsTable.innerHTML = "";
   objectsArray.map((product)=> {
     const cardProductFill = `
@@ -41,90 +42,3 @@ const loadTable = (objectsArray) => {
     productsTable.insertAdjacentHTML("beforeend", cardProductFill)
   })
 }
-loadTable(products);
-
-
-
-
-const inputNameFilter = document.querySelector(".main__inputNameFilter");
-const inputCategoryFilter = document.querySelector(".main__inputCategoryFilter");
-const btnFilterClear = document.querySelector(".main__btnFilterClear")
-const tableMsjNotFound = `<div class="w-100 vh-100 text-center"> <h2 class="text-secondary">Oops, no hay productos que coincidan con su busqueda</h2></div>`;
-// console.log(inputCategoryFilter.parentNode.innerHTML)
-
-
-const renderCategoryFilter = () => {
-  inputCategoryFilter.innerHTML = ``;
-  inputCategoryFilter.insertAdjacentHTML("beforeend", `<option disabled selected hidden class="default" value=""> Filtrar por categoria </option>`)
-  const categoryFilter = products.map((prod)=>{
-    const cat = prod.category.trim().charAt().toUpperCase()+prod.category.slice(1);
-    return cat;
-  });
-  const categories = [...new Set(categoryFilter)];
-  categories.forEach( cat => {
-    inputCategoryFilter.insertAdjacentHTML("beforeend", `<option value="${cat}">${cat}</option>`)
-  })  
-}
-renderCategoryFilter()
-
-
-const renderFilteredProducts = (inputName, inputCategory) => {
-  console.log(inputName, inputCategory);
-  let filteredProducts = nameProductFilter(inputName);
-  console.log(filteredProducts);
-  filteredProducts = categoryProductFilter(inputCategory, filteredProducts);
-  console.log(filteredProducts);
-  filteredProducts!="" ? loadTable(filteredProducts) : productsTable.innerHTML = tableMsjNotFound;
-}
-
-const nameProductFilter = (inputWord) => {
-  products = getProducts();
-  let outgoing = [];
-  const amendedInputWord = inputWord.toString().toLowerCase().trim();
-  if (amendedInputWord == "") {
-    outgoing = products
-  } else {
-    products.forEach( product => {
-    const amendedProductName = product.title.toLowerCase();
-    amendedProductName.includes(amendedInputWord) ? outgoing.push(product) : null
-  })
-  }
-  console.log(outgoing);
-  return outgoing;
-};
-
-const categoryProductFilter = (selectedCategory, productArray) => {
-  console.log(selectedCategory);
-  console.log(productArray);
-  const outgoing = productArray.filter((product) => product.category.toLowerCase() == selectedCategory.toLowerCase());
-  console.log(outgoing);
-  if (selectedCategory) {
-    console.log(selectedCategory);
-    return outgoing 
-  } else {
-    return productArray;
-  }
-};
-
-inputNameFilter.addEventListener("keyup", (e) => {
-  renderFilteredProducts(
-    e.target.value.toLowerCase(),
-    inputCategoryFilter.value.toLowerCase()
-  );
-  console.log(e.target.value.toLowerCase())
-});
-
-inputCategoryFilter.addEventListener("change", (e) => {
-  renderFilteredProducts(
-    inputNameFilter.value.toLowerCase(),
-    e.target.value.toLowerCase()
-  );
-  console.log(e.target.value.toLowerCase())
-});
-
-btnFilterClear.addEventListener("click", (e) => {
-  e.preventDefault();
-  inputNameFilter.value = "";
-  inputCategoryFilter.value = "";
-  loadTable(products)
-});
