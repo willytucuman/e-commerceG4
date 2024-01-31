87def2f8e88bd74df8bb346ca9adebcb51b7de6e
jeremiasjm <jeremiasjuarezmarrades15@gmail.com>
2024-01-31 00:30:39 -0300

    agrego atributos  y cambio de estilos

--- js/classes/product.class.js
+++ js/classes/product.class.js
@@ -1,11 +1,33 @@
 export class Product {
-    constructor(id, image, title, description, price, category,stock) {
-      this.id = id;
-      this.image = image;
-      this.title = title;
-      this.description = description;
-      this.price = price;
-      this.category = category;
-      this.stock = stock; 
-    }
-  } 
+  constructor(
+    id,
+    image,
+    title,
+    description,
+    price,
+    category,
+    stock,
+    detail1,
+    detail2,
+    detail3,
+    detail4,
+    medida1,
+    marca,
+    offer
+  ) {
+    this.id = id;
+    this.image = image;
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.category = category;
+    this.stock = stock;
+    this.detail1 = detail1;
+    this.detail2 = detail2;
+    this.detail3 = detail3;
+    this.detail4 = detail4;
+    this.medida1 = medida1;
+    this.marca = marca;
+    this.offer = offer;
+  }
+}
